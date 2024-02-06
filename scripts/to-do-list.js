eac6a4b2fcade683ba9d0e9aaa850fe6ae5b2adc
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-02-05 21:07:17 -0600

    improving code

--- scripts/to-do-list.js
+++ scripts/to-do-list.js
@@ -1,7 +1,20 @@
-const toDo = [];
+const toDo = ['make dinner', 'wash dishes'];
 
+renderTodoList();
 
+function renderTodoList (){
 
+   let toDoHTML = '';
+
+   for (let i = 0; i < toDo.length; i++){
+      const makeIt = toDo[i];
+      const html = `<p>${makeIt}</p>`;
+      toDoHTML += html;
+   }
+   console.log(toDoHTML)
+
+   document.querySelector('.js-toDo-list').innerHTML = toDoHTML;
+}
 function addTodo(){
    const inputElement = document.querySelector('.js-name-input');
    const name = inputElement.value;
@@ -11,5 +24,5 @@ function addTodo(){
 
    inputElement.value = '';
 
-   
+   renderTodoList();
 };
