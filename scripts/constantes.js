5f4a19e6037116246f6fe0342776e80a028992a1
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-04-29 01:32:02 -0600

    modulos, import-export

---
+++ scripts/constantes.js
@@ -0,0 +1,11 @@
+export const PI = Math.PI;
+
+export  let usuario = "MAnuel"
+export  let password = "QWERTY"
+
+const hello = () => console.log("Hola")
+
+
+export default function salida(){
+console.log("Hola modulos")
+}
---
+++ scripts/modulos.js
@@ -0,0 +1,10 @@
+import saludar, {PI, usuario} from "./constantes.js";
+import { aritmetica} from "./aritmetica.js";
+
+console.log("archivos modulos Js")
+
+console.log(PI,usuario)
+
+console.log(aritmetica.sumar(3,4))
+
+saludar();
