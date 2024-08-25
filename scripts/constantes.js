6635d1294d54b8f3f27edad5ae84968cc8259842
Manuelb3nvenuto <91238203+Manuelb3nvenuto@users.noreply.github.com>
2024-08-24 21:14:58 -0600

    descriptions

--- scripts/constantes.js
+++ scripts/constantes.js
@@ -1,11 +1,31 @@
 export const PI = Math.PI;
 
-export  let usuario = "MAnuel"
-export  let password = "QWERTY"
+export  let usuario = "MAnuel";
+export  let password = "QWERTY";
 
+
+//funcion expresada aquella que se guarda en variable , en este caso no se logra ejecutar porque se llama antes de la expresion
 const hello = () => console.log("Hola")
 
-
-export default function salida(){
+//default hace que se mande a llamar de manera automatica 
+//expresion declarada always better
+export default function saludar(){
 console.log("Hola modulos")
 }
+
+export class despedida{
+    constructor(){
+    console.log("Adios modulos")
+    }
+}
+
+
+
+/*
+//Resumen Clases: Se instancian con new. No se pueden llamar sin new. Funciones Normales: No requieren new para ser llamadas.
+
+export function despedida(){
+    console.log("Adios modulos")
+    }
+//decarar y despues llamar , es una mejor practica
+*/
